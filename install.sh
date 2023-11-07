b4c8212931ee3219b2f9dafc4135760e92cb5736
m1faridi <m1faridii@gmail.com>
2023-11-07 16:16:11 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,44 +4,67 @@ cat << 'EOF' > serverInfoV1.go
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "log"
     "net/http"
     "os/exec"
-    "strconv" 
+    "strconv"
+    "strings"
 )
 
+// تعریف ساختار برای فرمت خروجی JSON
+type IPList struct {
+    IPs []string `json:"ips"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-   port := r.URL.Query().Get("port")
+    port := r.URL.Query().Get("port")
+
+    // اعتبارسنجی ساده برای پورت
     if _, err := strconv.Atoi(port); err != nil {
         http.Error(w, "Invalid port", http.StatusBadRequest)
         return
     }
 
-    cmdString := fmt.Sprintf(`sudo netstat -anp | grep ':%s' | grep ESTABLISHED | awk '{print "{\"ip\": \""$5"\"}"}' | sort  | uniq | jq -s .`, port)
+    cmdString := fmt.Sprintf(`sudo netstat -anp | grep ':%s' | grep ESTABLISHED | awk '{print $5}' | cut -d':' -f1 | sort | uniq`, port)
     out, err := exec.Command("bash", "-c", cmdString).Output()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
 
+    // تبدیل خروجی به رشته و سپس تقسیم آن به خطوط
+    outputLines := strings.Split(string(out), "\n")
 
-    
-    //cmd := `sudo netstat -anp | grep ':8080' | grep ESTABLISHED | awk '{print "{\"ip\": \""$5"\"}"}' | sort | uniq | jq -s . `
-    //out, err := exec.Command("bash", "-c", cmd).Output()
-    //if err != nil {
-    //    http.Error(w, err.Error(), http.StatusInternalServerError)
-    //    return
-    //}
-    fmt.Fprintf(w, "%s", out)
+    // ایجاد یک نمونه از ساختار IPList
+    ipList := IPList{}
+
+    // انتخاب فقط خطوط غیر خالی و ذخیره آنها در ساختار IPList
+    for _, line := range outputLines {
+        if line != "" {
+            ipList.IPs = append(ipList.IPs, line)
+        }
+    }
+
+    // فرمت به JSON
+    jsonOutput, err := json.Marshal(ipList)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    fmt.Fprintf(w, "%s", jsonOutput)
 }
 
 func main() {
-    http.HandleFunc("/netstat", handler) 
+    http.HandleFunc("/netstat", handler)
 
     fmt.Println("Server is running on port 8891...")
-    log.Fatal(http.ListenAndServe(":8891", nil)) 
+    log.Fatal(http.ListenAndServe(":8891", nil))
 }
+
 EOF
 
 
