ec0b5c924776e3ae9b7491060e34e9c33c5d3d2b
m1faridi <m1faridii@gmail.com>
2023-11-07 15:43:03 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,12 +11,27 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    cmd := `sudo netstat -anp | grep ':8080' | grep ESTABLISHED | awk '{print "{\"ip\": \""$5"\"}"}' | sort | uniq | jq -s . `
-    out, err := exec.Command("bash", "-c", cmd).Output()
+   port := r.URL.Query().Get("port")
+    if _, err := strconv.Atoi(port); err != nil {
+        http.Error(w, "Invalid port", http.StatusBadRequest)
+        return
+    }
+
+    cmdString := fmt.Sprintf(`sudo netstat -anp | grep ':%s' | grep ESTABLISHED | awk '{print "{\"ip\": \""$5"\"}"}' | sort  | uniq | jq -s .`, port)
+    out, err := exec.Command("bash", "-c", cmdString).Output()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+
+
+    
+    //cmd := `sudo netstat -anp | grep ':8080' | grep ESTABLISHED | awk '{print "{\"ip\": \""$5"\"}"}' | sort | uniq | jq -s . `
+    //out, err := exec.Command("bash", "-c", cmd).Output()
+    //if err != nil {
+    //    http.Error(w, err.Error(), http.StatusInternalServerError)
+    //    return
+    //}
     fmt.Fprintf(w, "%s", out)
 }
 
