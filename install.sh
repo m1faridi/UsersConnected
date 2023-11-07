a6c7e213427ea57339b099b99790f3a39d9f1bdd
m1faridi <m1faridii@gmail.com>
2023-11-07 18:00:57 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,120 @@
 sudo apt install golang-go
 
 cat << 'EOF' > serverInfoV1.go
-package main
-
 import (
+    "bufio"
     "encoding/json"
     "fmt"
     "log"
     "net/http"
+    "os"
     "os/exec"
     "strconv"
     "strings"
+    "syscall"
+    "time"
+ 
 )
 
-// تعریف ساختار برای فرمت خروجی JSON
-type IPList struct {
-    IPs []string `json:"ips"`
+type SystemInfo struct {
+    IPs          []string `json:"ips"`
+    TotalRAM     uint64   `json:"total_ram_bytes"`
+    UsedRAM      uint64   `json:"used_ram_bytes"`
+    CPUUsage     float64  `json:"cpu_usage_percent"`
+    CPUCoreCount int      `json:"cpu_core_count"`
+}
+
+func getRAMInfo() (uint64, uint64, error) {
+    var stat syscall.Sysinfo_t
+    err := syscall.Sysinfo(&stat)
+    if err != nil {
+        return 0, 0, err
+    }
+
+    totalRamBytes := stat.Totalram * uint64(stat.Unit)
+    usedRamBytes := (stat.Totalram - stat.Freeram) * uint64(stat.Unit)
+    return totalRamBytes, usedRamBytes, nil
+}
+
+func getCPUCoreCount() (int, error) {
+    file, err := os.Open("/proc/cpuinfo")
+    if err != nil {
+        return 0, err
+    }
+    defer file.Close()
+
+    coreCount := 0
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        if strings.HasPrefix(scanner.Text(), "processor") {
+            coreCount++
+        }
+    }
+
+    if err := scanner.Err(); err != nil {
+        return 0, err
+    }
+
+    return coreCount, nil
+}
+
+func getCPUUsage() (float64, error) {
+    idleTime1, totalTime1, err := readCPUStat()
+    if err != nil {
+        return 0, err
+    }
+
+    time.Sleep(500 * time.Millisecond)
+
+    idleTime2, totalTime2, err := readCPUStat()
+    if err != nil {
+        return 0, err
+    }
+
+    idleDelta := idleTime2 - idleTime1
+    totalDelta := totalTime2 - totalTime1
+
+    cpuUsage := 100.0 * (1.0 - float64(idleDelta)/float64(totalDelta))
+    return cpuUsage, nil
+}
+
+func readCPUStat() (idleTime, totalTime int64, err error) {
+    file, err := os.Open("/proc/stat")
+    if err != nil {
+        return 0, 0, err
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    if !scanner.Scan() {
+        return 0, 0, fmt.Errorf("failed to read /proc/stat")
+    }
+
+    fields := strings.Fields(scanner.Text())
+    if len(fields) < 5 {
+        return 0, 0, fmt.Errorf("unexpected format in /proc/stat")
+    }
+
+    var total int64
+    for _, field := range fields[1:] {
+        val, err := strconv.ParseInt(field, 10, 64)
+        if err != nil {
+            return 0, 0, err
+        }
+        total += val
+    }
+
+    idle, err := strconv.ParseInt(fields[4], 10, 64)
+    if err != nil {
+        return 0, 0, err
+    }
+
+    return idle, total, nil
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     port := r.URL.Query().Get("port")
 
-    // اعتبارسنجی ساده برای پورت
     if _, err := strconv.Atoi(port); err != nil {
         http.Error(w, "Invalid port", http.StatusBadRequest)
         return
@@ -34,21 +127,35 @@ func handler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    // تبدیل خروجی به رشته و سپس تقسیم آن به خطوط
     outputLines := strings.Split(string(out), "\n")
 
-    // ایجاد یک نمونه از ساختار IPList
-    ipList := IPList{}
+    systemInfo := SystemInfo{}
 
-    // انتخاب فقط خطوط غیر خالی و ذخیره آنها در ساختار IPList
     for _, line := range outputLines {
         if line != "" {
-            ipList.IPs = append(ipList.IPs, line)
+            systemInfo.IPs = append(systemInfo.IPs, line)
         }
     }
 
-    // فرمت به JSON
-    jsonOutput, err := json.Marshal(ipList)
+    systemInfo.TotalRAM, systemInfo.UsedRAM, err = getRAMInfo()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    systemInfo.CPUCoreCount, err = getCPUCoreCount()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    systemInfo.CPUUsage, err = getCPUUsage()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    jsonOutput, err := json.Marshal(systemInfo)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -64,7 +171,6 @@ func main() {
     fmt.Println("Server is running on port 8891...")
     log.Fatal(http.ListenAndServe(":8891", nil))
 }
-
 EOF
 
 
