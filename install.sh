38dcd534f85baa81ced381a9eea2f41014729805
m1faridi <m1faridii@gmail.com>
2023-11-07 14:46:42 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    awkScript := "{print \\\"{\\\\\\\"local_address\\\\\\\": \\\\\\\"\\\"$4\\\"\\\\\\\", \\\\\\\"foreign_address\\\\\\\": \\\\\\\"\\\"$5\\\"\\\\\\\", \\\\\\\"process\\\\\\\": \\\\\\\"\\\"$7\\\"\\\\\\\"}\\\"}"
-    cmd := "sudo netstat -anp | grep ':8080' | grep ESTABLISHED | awk '" + awkScript + "' | jq -s ."
+    cmd := "sudo netstat -anp | grep ':8080' | grep ESTABLISHED"
 
     out, err := exec.Command("bash", "-c", cmd).Output()
     if err != nil {
